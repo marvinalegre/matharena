c2601876e0acec7bd1f03e3b3795d9800a32e7b1
Marvin Alegre <marvinalegredev@gmail.com>
2026-08-20 11:29:54 +0800

    add users table schema

---
+++ migrations/0001_create_users.sql
@@ -0,0 +1,7 @@
+-- Migration number: 0001 	 2026-08-20T03:05:29.604Z
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT NOT NULL UNIQUE,
+  password_hash TEXT NOT NULL,
+  created_at TEXT NOT NULL DEFAULT (strftime ('%Y-%m-%dT%H:%M:%fZ', 'now'))
+);
