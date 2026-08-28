73ac3b0aeee0210774153c5879d0eac2a237bd14
Marvin Alegre <marvinalegredev@gmail.com>
2026-08-28 08:37:58 +0800

    fix not redirecting auth pages

--- public/js/pages/signup.js
+++ public/js/pages/signup.js
@@ -1,4 +1,4 @@
-document.getElementById("signup-form").addEventListener("fx:after", (evt) => {
+document.body.addEventListener("fx:after", (evt) => {
   const redirect = evt.detail.cfg.response.headers.get("FX-Redirect");
   if (redirect) {
     window.location.href = redirect;
