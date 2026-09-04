e38016c94c84c02a310a8ae0e5dcc738e273b6d2
Marvin Alegre <marvinalegredev@gmail.com>
2026-09-04 17:46:34 +0800

    support addition-single-digit-carry question

---
+++ src/lib/supportedQuestions.ts
@@ -0,0 +1,4 @@
+export const SUPPORTED_QUESTIONS = [
+  "addition-single-digit-no-carry",
+  "addition-single-digit-carry",
+] as const;
--- src/routes/playRoutes.tsx
+++ src/routes/playRoutes.tsx
@@ -2,17 +2,17 @@ import { Hono } from "hono";
 
 import { forge } from "mathforge";
 import type { AppEnv } from "@/types/env";
+import { SUPPORTED_QUESTIONS } from "@/lib/supportedQuestions";
 import { PlayPage } from "@/pages/PlayPage";
 import { QuestionForm } from "@/components/QuestionForm";
 
 export const playRoutes = new Hono<AppEnv>();
 
 playRoutes.get("/", (c) => {
-  const question = forge("addition-single-digit-no-carry");
-  const formattedQuestion = formatQuestion(
-    "addition-single-digit-no-carry",
-    question.data,
-  );
+  const randomQuestionCode =
+    SUPPORTED_QUESTIONS[Math.floor(Math.random() * SUPPORTED_QUESTIONS.length)];
+  const question = forge(randomQuestionCode);
+  const formattedQuestion = formatQuestion(randomQuestionCode, question.data);
 
   if (!c.get("user")) {
     return c.html(
@@ -39,11 +39,12 @@ playRoutes.post("/", async (c) => {
       throw new Error("BOOOOOOOOOOOOOOMMMMMMMM!!!");
     }
 
-    const question = forge("addition-single-digit-no-carry");
-    const formattedQuestion = formatQuestion(
-      "addition-single-digit-no-carry",
-      question.data,
-    );
+    const randomQuestionCode =
+      SUPPORTED_QUESTIONS[
+        Math.floor(Math.random() * SUPPORTED_QUESTIONS.length)
+      ];
+    const question = forge(randomQuestionCode);
+    const formattedQuestion = formatQuestion(randomQuestionCode, question.data);
 
     return c.html(
       <QuestionForm
@@ -65,6 +66,7 @@ playRoutes.post("/", async (c) => {
 function formatQuestion(code: string, data: any) {
   switch (code) {
     case "addition-single-digit-no-carry":
+    case "addition-single-digit-carry":
       return `\\[${data.a} + ${data.b} = \\; ?\\]`;
   }
 
