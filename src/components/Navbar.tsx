cada9537e2070ef5ac81ec69663040a37a144a02
Marvin Alegre <marvinalegredev@gmail.com>
2026-09-07 12:36:05 +0800

    add user badge

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,8 +25,7 @@ export const Navbar: FC<NavbarProps> = ({ currentPath }) => {
         </a>
       </nav>
 
-      <button class="navbar__user-badge">user</button>
-      <div className="user-badge">M</div>
+      <button class="navbar__user-badge">M</button>
 
       <div id="nav-overlay" class="navbar__overlay"></div>
     </header>
