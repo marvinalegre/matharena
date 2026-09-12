be9924ce16b1781bb5be23d70efd44e67ff80a36
Marvin Alegre <marvinalegredev@gmail.com>
2026-09-12 17:06:58 +0800

    disable form controls on answer submission

--- public/js/pages/play.js
+++ public/js/pages/play.js
@@ -63,3 +63,24 @@ document.addEventListener("fx:config", (evt) => {
     }
   };
 });
+
+// fixi disable elements extension
+document.addEventListener("fx:init", (evt) => {
+  if (evt.target.matches("[ext-fx-disable]")) {
+    evt.target.addEventListener("fx:before", () => {
+      const disableTargets = document.querySelectorAll(
+        "[ext-fx-disable-target]",
+      );
+      disableTargets.forEach((target) => {
+        target.disabled = true;
+      });
+      evt.target.addEventListener("fx:after", (afterEvt) => {
+        if (afterEvt.target == evt.target) {
+          disableTargets.forEach((target) => {
+            target.disabled = false;
+          });
+        }
+      });
+    });
+  }
+});
--- src/components/QuestionForm.tsx
+++ src/components/QuestionForm.tsx
@@ -7,10 +7,19 @@ export const QuestionForm = ({ question, answer }: PlayPageProps) => (
     fx-method="post"
     fx-target="#target"
     fx-swap="outerHTML"
+    ext-fx-disable
   >
     <p id="question">{question}</p>
-    <input name="answer" placeholder="Answer" autocomplete="off" required />
+    <input
+      name="answer"
+      placeholder="Answer"
+      autocomplete="off"
+      required
+      ext-fx-disable-target
+    />
     {answer && <input type="hidden" name="correctAnswer" value={answer} />}
-    <button type="submit">Submit</button>
+    <button type="submit" ext-fx-disable-target>
+      Submit
+    </button>
   </form>
 );
